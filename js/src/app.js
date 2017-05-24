0c59ead44330b244e3cc523f02f6bb4090e073ed
Jonathan Pollock <jon.pollock@me.com>
2017-05-24 16:24:22 -0400

    Added Graph to Index

--- js/src/app.js
+++ js/src/app.js
@@ -1,3 +1,104 @@
 var name = 'carlosjunod'
-
 console.log(`my name is ${name}`)
+
+
+/// Analytics Code  ////////////////////////////////////////////////////////
+
+let ctx = document.getElementById("weekChart").getContext('2d');
+let ctx2 = document.getElementById("monthChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+var myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ["January", "Febuary", "March", "April", "May", "June"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+function addValuesChart(labels, values) {
+  myChart.data.datasets[0].labels = values;
+  myChart.data.datasets[0].data = labels;
+  myChart.update();
+}
+
+function addValuesChart2(values, labels) {
+  myChart2.data.datasets[0].labels = values;
+  myChart2.data.datasets[0].data = labels;
+  myChart2.update();
+}
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
